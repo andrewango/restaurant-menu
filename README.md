a0ff97eaf35d911c06edb2304977af9109ea8552
ANDREW NGO <ango@udel.edu>
2023-04-06 20:15:10 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Restaurant Menu
+
+A CISC275 Project by ...
+
+- Gavin Caulfield
+- Ella Wilkins
+- Colby Frashure
+- Sehee Hwang
+- Andrew Ngo
---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./App.css";
+import { ChangeType } from "./components/ChangeType";
+import { RevealAnswer } from "./components/RevealAnswer";
+import { StartAttempt } from "./components/StartAttempt";
+import { TwoDice } from "./components/TwoDice";
+import { CycleHoliday } from "./components/CycleHoliday";
+import { Counter } from "./components/Counter";
+import { DoubleHalf } from "./bad-components/DoubleHalf";
+import { ColoredBox } from "./bad-components/ColoredBox";
+import { ShoveBox } from "./bad-components/ShoveBox";
+import { ChooseTeam } from "./bad-components/ChooseTeam";
+import { CheckAnswer } from "./form-components/CheckAnswer";
+import { GiveAttempts } from "./form-components/GiveAttempts";
+import { EditMode } from "./form-components/EditMode";
+import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
+import { ChangeColor } from "./form-components/ChangeColor";
+
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <header className="App-header">
+                UD CISC275 with React Hooks and TypeScript - Andrew Ngo
+            </header>
+            <hr></hr>
+            <CheckAnswer expectedAnswer="42"></CheckAnswer>
+            <hr></hr>
+            <GiveAttempts></GiveAttempts>
+            <hr></hr>
+            <EditMode></EditMode>
+            <hr></hr>
+            <ChangeColor></ChangeColor>
+            <hr></hr>
+            <MultipleChoiceQuestion
+                options={["a", "b", "c"]}
+                expectedAnswer="b"
+            ></MultipleChoiceQuestion>
+            <hr></hr>
+            <DoubleHalf></DoubleHalf>
+            <hr></hr>
+            <ChooseTeam></ChooseTeam>
+            <hr></hr>
+            <ColoredBox></ColoredBox>
+            <hr></hr>
+            <ShoveBox></ShoveBox>
+            <hr></hr>
+            <Counter></Counter>
+            <hr></hr>
+            <RevealAnswer></RevealAnswer>
+            <hr></hr>
+            <StartAttempt></StartAttempt>
+            <hr></hr>
+            <TwoDice></TwoDice>
+            <hr></hr>
+            <ChangeType></ChangeType>
+            <hr></hr>
+            <CycleHoliday></CycleHoliday>
+            <hr></hr>
+        </div>
+    );
+}
+
+export default App;
