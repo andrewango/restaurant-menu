d4379463c295df55e7d3d58bd1d94a7d0177b113
ANDREW NGO <ango@udel.edu>
2023-04-10 17:38:12 -0400

    Moved directories into src and added member names to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,20 @@ import "./App.css";
 
 function App(): JSX.Element {
     return (
-        <div className="App">
-            <header className="App-header">Restaurant Menu</header>
-        </div>
+        <>
+            <div className="App">
+                <header className="App-header">Restaurant Menu</header>
+            </div>
+            <div>
+                <ul>
+                    <li>Gavin Caulfield</li>
+                    <li>Ella Wilkins</li>
+                    <li>Colby Frashure</li>
+                    <li>Sehee Hwang</li>
+                    <li>Andrew Ngo</li>
+                </ul>
+            </div>
+        </>
     );
 }
 
