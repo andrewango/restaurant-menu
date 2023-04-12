245ae077b91214a2d0edcea3660525af14c2ef92
ANDREW NGO <ango@udel.edu>
2023-04-12 16:43:10 -0400

    Implemented hash routing pages and Landing page UI

--- src/pages/aboutus.tsx
+++ src/pages/aboutus.tsx
@@ -1,6 +1,12 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Heading, Container } from "@chakra-ui/react";
 
 export default function AboutUs() {
-    return <div>test</div>;
+    return (
+        <div style={{ padding: 10 }}>
+            <Container>
+                <Heading>Information About Us!</Heading>
+            </Container>
+        </div>
+    );
 }
