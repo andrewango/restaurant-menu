bafd4723904d28098c16f0e9f861da6e6e03328f
Andrew Ngo <ango@udel.edu>
2023-04-13 00:08:01 -0400

    working on dragging the food items

--- src/components/ItemListUI.tsx
+++ src/components/ItemListUI.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { VStack, Box } from "@chakra-ui/react";
 import foodData from "../data/foods.json";
 import RatingFeature from "./RatingFeature";
+import { DndProvider, useDrag } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 export default function ItemListUI() {
     const foodList = foodData.FOODS.map((food) => {
@@ -12,26 +14,37 @@ export default function ItemListUI() {
         };
     });
 
+    const [{ isDragging }, drag] = useDrag({
+        type: "box",
+        collect: (monitor) => ({
+            isDragging: monitor.isDragging()
+        })
+    });
+
     return (
-        <VStack spacing="3px" mt={100}>
-            {foodList.map((food) => {
-                return (
-                    <Box
-                        key={food.name}
-                        w={300}
-                        textAlign="center"
-                        borderWidth={2}
-                        borderColor="black"
-                        borderRadius="md"
-                        p={2}
-                    >
-                        <b>{food.name}</b>
-                        <br></br>
-                        {food.desc}
-                        <RatingFeature></RatingFeature>
-                    </Box>
-                );
-            })}
-        </VStack>
+        <DndProvider backend={HTML5Backend}>
+            <VStack spacing="3px" mt={100}>
+                {foodList.map((food) => {
+                    return (
+                        <Box
+                            key={food.name}
+                            w={300}
+                            textAlign="center"
+                            borderWidth={2}
+                            borderColor="black"
+                            borderRadius="md"
+                            p={2}
+                            ref={drag}
+                            opacity={isDragging ? 0.5 : 1.0}
+                        >
+                            <b>{food.name}</b>
+                            <br></br>
+                            {food.desc}
+                            <RatingFeature></RatingFeature>
+                        </Box>
+                    );
+                })}
+            </VStack>
+        </DndProvider>
     );
 }
