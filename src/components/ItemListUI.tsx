3ee61eba1c612c653363d52d051b9a2a94827f80
gavincaul <gavcaul@udel.edu>
2023-04-19 17:55:03 -0400

    Added scroll for the foods, and "stickied" nav bar

--- src/components/ItemListUI.tsx
+++ src/components/ItemListUI.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { VStack } from "@chakra-ui/react";
+import { VStack, Box } from "@chakra-ui/react";
 import { foodProps } from "../interfaces/Food";
-//import { DeliveryDropDown } from "./DeliveryDropDown";
 import FoodItem from "./FoodItem";
+
 export default function ItemListUI({
     foodData
 }: {
@@ -17,18 +17,20 @@ export default function ItemListUI({
     });
     // Maps each food in our list to a box with food details
     return (
-        <VStack spacing="3px" mt={100}>
-            {foodList.map((food) => {
-                return (
-                    <FoodItem
-                        key={food.name}
-                        name={food.name}
-                        image={food.image}
-                        desc={food.desc}
-                        ingredients={food.ingredients}
-                    ></FoodItem>
-                );
-            })}
-        </VStack>
+        <Box maxH="500px" overflowY="scroll" mt={100}>
+            <VStack spacing="3px">
+                {foodList.map((food) => {
+                    return (
+                        <FoodItem
+                            key={food.name}
+                            name={food.name}
+                            image={food.image}
+                            desc={food.desc}
+                            ingredients={food.ingredients}
+                        />
+                    );
+                })}
+            </VStack>
+        </Box>
     );
 }
