30058a6cfde386b4e0009845a123690aba493ccd
cfrashure <colbyfrashure@gmail.com>
2023-04-26 21:56:16 -0400

    added button functionality and more styling to make ui more consistent and plesant

--- src/components/DeliveryDropDown.tsx
+++ src/components/DeliveryDropDown.tsx
@@ -1,7 +1,9 @@
 import { Box } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
-import { Button } from "react-bootstrap";
+import { Button } from "@chakra-ui/react";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+//import styled from "styled-components";
 export function DeliveryDropDown(): JSX.Element {
     const [time, changeTime] = useState<string>("");
     const TIMES = [
@@ -16,8 +18,20 @@ export function DeliveryDropDown(): JSX.Element {
         "8:00",
         "9:00"
     ];
+    /*
+    const Button = styled.button`
+        background-color: #ef5350;
+        color: white;
+        font-size: 20px;
+        padding: 10px 60px;
+        border-radius: 5px;
+        margin: 10px 0px;
+        cursor: pointer;
+        transition="background-color 0.3s ease"
+    `;
+    */
     return (
-        <Box width="90%">
+        <Box width="90%" position="absolute" bottom="0" mb="3">
             <p
                 className="del-time"
                 style={{ color: "black", textAlign: "left" }}
@@ -28,7 +42,10 @@ export function DeliveryDropDown(): JSX.Element {
                 <form>
                     <Form.Group controlId="delivery-dropdown">
                         <Form.Select
-                            style={{ width: "160px" }}
+                            style={{
+                                width: "160px",
+                                marginRight: "380px"
+                            }}
                             onChange={(e) => changeTime(e.target.value)}
                             name={time}
                             value={time}
@@ -43,7 +60,23 @@ export function DeliveryDropDown(): JSX.Element {
                         </Form.Select>
                     </Form.Group>
                 </form>
-                <Button style={{ marginLeft: "28rem" }}>Checkout</Button>
+                <Button
+                    bg="red.400"
+                    as="span"
+                    fontWeight="bold"
+                    color="white"
+                    rightIcon={<AiOutlineShoppingCart />}
+                    px={1}
+                    py={1}
+                    transition="background-color 0.3s ease"
+                    _hover={{ bgColor: "red.600", color: "white" }}
+                    borderRadius="md"
+                    onClick={() =>
+                        alert("Ordered placed! Your delivery time is " + time)
+                    }
+                >
+                    Checkout
+                </Button>
             </div>
         </Box>
     );
