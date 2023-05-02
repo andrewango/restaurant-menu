af506e253d27a5bad6d16ea12ebe62fb300adefa
cfrashure <colbyfrashure@gmail.com>
2023-05-02 17:34:13 -0400

    changed ownerlanding to display 3 in a row, similar to the ManageFood page

--- src/pages/OwnerLanding.tsx
+++ src/pages/OwnerLanding.tsx
@@ -48,14 +48,14 @@ export default function OwnerLanding() {
             <Center mt={10}>
                 <Grid
                     templateColumns={
-                        isLargerThan2000 ? "repeat(2, 1fr)" : "repeat(2, 1fr)"
+                        isLargerThan2000 ? "repeat(3, 1fr)" : "repeat(3, 1fr)"
                     }
-                    columnGap={window.innerWidth * 0.05}
+                    columnGap={window.innerWidth * 0.03}
                 >
                     <Button
                         className="food"
                         //backgroundImage={background}
-                        w={window.innerWidth * 0.4}
+                        w={window.innerWidth * 0.25}
                         h={window.innerHeight * 0.8}
                         onClick={() => (location.hash = "/ManageFoods")}
                     >
@@ -70,7 +70,7 @@ export default function OwnerLanding() {
                     <Button
                         className="user"
                         //backgroundImage={manageUsers}
-                        w={window.innerWidth * 0.4}
+                        w={window.innerWidth * 0.25}
                         h={window.innerHeight * 0.8}
                         onClick={() => (location.hash = "/EditUsers")}
                     >
@@ -81,12 +81,12 @@ export default function OwnerLanding() {
                     <Button
                         className="stats"
                         //backgroundImage={manageUsers}
-                        w={window.innerWidth * 0.4}
+                        w={window.innerWidth * 0.25}
                         h={window.innerHeight * 0.8}
                         onClick={() => (location.hash = "/UserStats")}
                     >
                         <Text className="text" font-weight="bold">
-                            Manage User
+                            View Stats
                         </Text>
                     </Button>
                 </Grid>
