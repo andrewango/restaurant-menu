e331b4ff909aa1c02522b28b615b4225e83117f4
Ella <ellawlk@udel.edu>
2023-05-09 16:06:35 -0400

    quick setup

---
+++ src/components/FoodItem.test.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import FoodItem from "./FoodItem";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+describe("FoodItem tests", () => {});
