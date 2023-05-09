11d812b62ec9b3a4fa512774d7cb08720caae499
cfrashure <colbyfrashure@gmail.com>
2023-05-09 14:12:10 -0400

    commit before pulling

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,115 @@
-import React from "react";
+import React, { ReactElement, ReactNode } from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { HashRouter, MemoryRouter } from "react-router-dom";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import MatchMediaMock from "jest-matchmedia-mock";
 
-test("renders learn react link", () => {
-    render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+new MatchMediaMock();
+
+const colors = {
+    brand: {
+        900: "#1a365d",
+        800: "#153e75",
+        700: "#2a69ac"
+    }
+};
+
+const theme = extendTheme({ colors });
+
+export const renderWithProviders = (ui: ReactElement) => {
+    const Wrapper = ({ children }: { children: ReactNode }) => (
+        <HashRouter>
+            <ChakraProvider theme={theme}>
+                <DndProvider backend={HTML5Backend}>{children}</DndProvider>
+            </ChakraProvider>
+        </HashRouter>
+    );
+
+    return render(ui, { wrapper: Wrapper });
+};
+
+describe("App", () => {
+    test("renders Landing page", () => {
+        renderWithProviders(
+            <MemoryRouter initialEntries={["/"]}>
+                <App />
+            </MemoryRouter>
+        );
+        expect(screen.getByTestId("landing-page")).toBeInTheDocument();
+    });
+
+    test("renders EditUsers page", () => {
+        renderWithProviders(
+            <MemoryRouter initialEntries={["/EditUsers"]}>
+                <App />
+            </MemoryRouter>
+        );
+        expect(screen.getByTestId("edit-users-page")).toBeInTheDocument();
+    });
+
+    test("renders EditFood page", () => {
+        renderWithProviders(
+            <MemoryRouter initialEntries={["/EditFood"]}>
+                <App />
+            </MemoryRouter>
+        );
+        expect(screen.getByTestId("edit-food-page")).toBeInTheDocument();
+    });
+
+    it("renders AboutUs page", () => {
+        renderWithProviders(
+            <MemoryRouter initialEntries={["/AboutUs"]}>
+                <App />
+            </MemoryRouter>
+        );
+        expect(screen.getByTestId("about-us-page")).toBeInTheDocument();
+    });
+
+    it("renders AddFood page", () => {
+        renderWithProviders(
+            <MemoryRouter initialEntries={["/AddFood"]}>
+                <App />
+            </MemoryRouter>
+        );
+        expect(screen.getByTestId("add-food-page")).toBeInTheDocument();
+    });
+
+    test("renders RemoveFood page", () => {
+        renderWithProviders(
+            <MemoryRouter initialEntries={["/RemoveFood"]}>
+                <App />
+            </MemoryRouter>
+        );
+        expect(screen.getByTestId("remove-food-page")).toBeInTheDocument();
+    });
+
+    test("renders OwnerLanding page", () => {
+        renderWithProviders(
+            <MemoryRouter initialEntries={["/OwnerLanding"]}>
+                <App />
+            </MemoryRouter>
+        );
+        expect(screen.getByTestId("owner-landing-page")).toBeInTheDocument();
+    });
+
+    test("renders ManageFoods page", () => {
+        renderWithProviders(
+            <MemoryRouter initialEntries={["/ManageFoods"]}>
+                <App />
+            </MemoryRouter>
+        );
+        expect(screen.getByTestId("manage-foods-page")).toBeInTheDocument();
+    });
+
+    test("renders UserStats page", () => {
+        renderWithProviders(
+            <MemoryRouter initialEntries={["/UserStats"]}>
+                <App />
+            </MemoryRouter>
+        );
+        expect(screen.getByTestId("user-stats-page")).toBeInTheDocument();
+    });
 });
--- src/components/AddDeleteUsers.test.tsx
+++ src/components/AddDeleteUsers.test.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import AddDeleteUsers from "./AddDeleteUsers";
 import { BrowserRouter } from "react-router-dom";
 //import { SelectRole } from "./SelectRole";
-//import userEvent from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 
 describe("AddDeleteUsers tests", () => {
+    beforeEach(() => {
+        sessionStorage.clear();
+    });
     test("The page is succesfully rendered", () => {
         render(
             <BrowserRouter>
@@ -15,7 +18,6 @@ describe("AddDeleteUsers tests", () => {
         expect(screen.getByText(/Name:/i)).toBeInTheDocument();
         expect(screen.getByText(/Add Customer/i)).toBeInTheDocument();
     });
-    /* We'll have to do this later.
     test("Adding a customer updates SelectRole", () => {
         render(
             <BrowserRouter>
@@ -24,11 +26,10 @@ describe("AddDeleteUsers tests", () => {
         );
         const textBox = screen.getByText(/Name:/i);
         userEvent.type(textBox, "Colby");
-        screen.getByText(/Add Customer/i).click();
+        fireEvent.click(screen.getByText("Add Customer"));
         const storage = sessionStorage.getItem("customers");
         console.log(storage);
         expect(storage).toContain("Colby");
         expect(screen.getByText(/Colby/i)).toBeInTheDocument();
     });
-    */
 });
