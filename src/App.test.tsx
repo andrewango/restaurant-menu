13921a55120ace00fd62448581b774212ea1ccd5
Andrew Ngo <ango@udel.edu>
2023-05-08 21:10:38 -0400

    Fixed conflicts with jest, React DnD, and Chakra UI

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { ReactElement, ReactNode } from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
+import { DndProvider } from "react-dnd";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import MatchMediaMock from "jest-matchmedia-mock";
 
-test("renders learn react link", () => {
-    render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
+new MatchMediaMock();
+
+const colors = {
+    brand: {
+        900: "#1a365d",
+        800: "#153e75",
+        700: "#2a69ac"
+    }
+};
+
+const theme = extendTheme({ colors });
+
+export const renderWithProviders = (ui: ReactElement) => {
+    const Wrapper = ({ children }: { children: ReactNode }) => (
+        <HashRouter>
+            <ChakraProvider theme={theme}>
+                <DndProvider backend={HTML5Backend}>{children}</DndProvider>
+            </ChakraProvider>
+        </HashRouter>
+    );
+
+    return render(ui, { wrapper: Wrapper });
+};
+
+test("renders the team member names somewhere", () => {
+    renderWithProviders(<App />);
+    const linkElement = screen.getByText(/about us/i);
     expect(linkElement).toBeInTheDocument();
 });
