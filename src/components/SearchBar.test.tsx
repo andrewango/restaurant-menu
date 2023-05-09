2373935e656a060d9704027005511a8f7196a168
Andrew Ngo <ango@udel.edu>
2023-05-09 00:20:08 -0400

    Created tests for SearchBar.tsx

--- src/components/SearchBar.test.tsx
+++ src/components/SearchBar.test.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement, ReactNode } from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { SearchBar } from "./SearchBar";
 import { HashRouter } from "react-router-dom";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
@@ -34,6 +34,114 @@ export const renderWithProviders = (ui: ReactElement) => {
 describe("SearchBar tests", () => {
     test("The Searchbar is succesfully rendered", () => {
         renderWithProviders(<SearchBar></SearchBar>);
-        expect(screen.queryByText(/Search Menu/i)).toBeInTheDocument();
+        expect(screen.getByTestId("searchbar")).toBeInTheDocument();
+    });
+
+    test("The customer is able to type in the searchbar and it updates values", () => {
+        renderWithProviders(<SearchBar></SearchBar>);
+        const searchInput = screen.getByTestId("search-input");
+        fireEvent.change(searchInput, { target: { value: "Pepperoni Pizza" } });
+        expect(searchInput).toHaveValue("Pepperoni Pizza");
+    });
+
+    test("Typing in the searchbar filters the menu list", () => {
+        renderWithProviders(<SearchBar></SearchBar>);
+        const searchInput = screen.getByLabelText("Search Menu");
+        fireEvent.change(searchInput, { target: { value: "Cheeseburger" } });
+        expect(searchInput).toHaveValue("Cheeseburger");
+        expect(screen.queryByText(/Cheeseburger/i)).toBeInTheDocument();
+        expect(screen.queryByText(/Pepperoni Pizza/i)).not.toBeInTheDocument();
+    });
+
+    test("Clicking on a Spicy tag filters the menu list correctly", () => {
+        renderWithProviders(<SearchBar></SearchBar>);
+        const spicyFilter = screen.getByTestId("spicy-filter-button");
+        fireEvent.click(spicyFilter);
+        expect(spicyFilter).toHaveStyle("backgroundColor: rgb(245, 101, 101");
+        const grid = screen.getByTestId("menu");
+        expect(grid.firstChild).toHaveTextContent("Spicy Salmon Avocado Sushi");
+        expect(grid.lastChild).toHaveTextContent("Koshari");
+    });
+
+    test("Clicking on a Popular tag filters the menu list correctly", () => {
+        renderWithProviders(<SearchBar></SearchBar>);
+        const popularFilter = screen.getByTestId("popular-filter-button");
+        fireEvent.click(popularFilter);
+        expect(popularFilter).toHaveStyle("backgroundColor: rgb(245, 101, 101");
+        const grid = screen.getByTestId("menu");
+        expect(grid.firstChild).toHaveTextContent("Pepperoni Pizza");
+        expect(grid.lastChild).toHaveTextContent("Flan");
+    });
+
+    test("Clicking on an Appetizer tag filters the menu list correctly", () => {
+        renderWithProviders(<SearchBar></SearchBar>);
+        const appetizerFilter = screen.getByTestId("appetizer-filter-button");
+        fireEvent.click(appetizerFilter);
+        expect(appetizerFilter).toHaveStyle(
+            "backgroundColor: rgb(245, 101, 101"
+        );
+        const grid = screen.getByTestId("menu");
+        expect(grid.firstChild).toHaveTextContent("House Salad");
+        expect(grid.lastChild).toHaveTextContent("Colcannon Puffs");
+    });
+
+    test("Clicking on an Entree tag filters the menu list correctly", () => {
+        renderWithProviders(<SearchBar></SearchBar>);
+        const entreeFilter = screen.getByTestId("entree-filter-button");
+        fireEvent.click(entreeFilter);
+        expect(entreeFilter).toHaveStyle("backgroundColor: rgb(245, 101, 101");
+        const grid = screen.getByTestId("menu");
+        expect(grid.firstChild).toHaveTextContent("Pepperoni Pizza");
+        expect(grid.lastChild).toHaveTextContent("Koshari");
+    });
+
+    test("Clicking on a Dessert tag filters the menu list correctly", () => {
+        renderWithProviders(<SearchBar></SearchBar>);
+        const dessertFilter = screen.getByTestId("dessert-filter-button");
+        fireEvent.click(dessertFilter);
+        expect(dessertFilter).toHaveStyle("backgroundColor: rgb(245, 101, 101");
+        const grid = screen.getByTestId("menu");
+        expect(grid.firstChild).toHaveTextContent("Tiramisu");
+        expect(grid.lastChild).toHaveTextContent("Flan");
+    });
+
+    test("Clicking on a Sort by High To Low Price radio button filters the menu list correctly", () => {
+        renderWithProviders(<SearchBar></SearchBar>);
+        const sortPriceFilter = screen.getByTestId(
+            "pricehightolow-filter-button"
+        );
+        fireEvent.click(sortPriceFilter);
+        expect(sortPriceFilter).toHaveStyle(
+            "backgroundColor: rgb(245, 101, 101"
+        );
+        const grid = screen.getByTestId("menu");
+        expect(grid.firstChild).toHaveTextContent("Paella");
+        expect(grid.lastChild).toHaveTextContent("House Salad");
+    });
+
+    test("Clicking on a Sort by Low To High Price radio button filters the menu list correctly", () => {
+        renderWithProviders(<SearchBar></SearchBar>);
+        const sortPriceFilter = screen.getByTestId(
+            "pricelowtohigh-filter-button"
+        );
+        fireEvent.click(sortPriceFilter);
+        expect(sortPriceFilter).toHaveStyle(
+            "backgroundColor: rgb(245, 101, 101"
+        );
+        const grid = screen.getByTestId("menu");
+        expect(grid.firstChild).toHaveTextContent("House Salad");
+        expect(grid.lastChild).toHaveTextContent("Paella");
+    });
+
+    test("Clicking on a Sort by Rating radio button filters the menu list correctly", () => {
+        renderWithProviders(<SearchBar></SearchBar>);
+        const sortRatingFilter = screen.getByTestId("rating-filter-button");
+        fireEvent.click(sortRatingFilter);
+        expect(sortRatingFilter).toHaveStyle(
+            "backgroundColor: rgb(245, 101, 101"
+        );
+        const grid = screen.getByTestId("menu");
+        expect(grid.firstChild).toHaveTextContent("Tacos al Pastor");
+        expect(grid.lastChild).toHaveTextContent("Hákarl");
     });
 });
