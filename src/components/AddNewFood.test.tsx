771965f7b51e8ddad2dc1ff2dc3dce3fbc64079d
Ella <ellawlk@udel.edu>
2023-05-09 22:22:25 -0400

    addded pre conditions

---
+++ src/components/AddNewFood.test.tsx
@@ -0,0 +1,32 @@
+import React, { ReactElement, ReactNode } from "react";
+import { render } from "@testing-library/react";
+import AddNewFood from "./AddNewFood";
+import { HashRouter } from "react-router-dom";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import MatchMediaMock from "jest-matchmedia-mock";
+export {};
+new MatchMediaMock();
+
+const colors = {
+    brand: {
+        900: "#1a365d",
+        800: "#153e75",
+        700: "#2a69ac"
+    }
+};
+
+const theme = extendTheme({ colors });
+
+export const renderWithProviders = (ui: ReactElement) => {
+    const Wrapper = ({ children }: { children: ReactNode }) => (
+        <HashRouter>
+            <ChakraProvider theme={theme}>
+                <DndProvider backend={HTML5Backend}>{children}</DndProvider>
+            </ChakraProvider>
+        </HashRouter>
+    );
+
+    return render(ui, { wrapper: Wrapper });
+};
